7ab3d62f303fa3e8c1b48f614793b6e4fb2a97e9
user <user@Ubuntu-VM>
2016-02-06 18:40:50 +0200

    Refactor

--- MessengerServerApp/src/Listener.cpp
+++ MessengerServerApp/src/Listener.cpp
@@ -1,11 +1,8 @@
-/*
- * Listener.cpp
- *
- *  Created on: Feb 6, 2016
- *      Author: user
- */
+#include <iostream>
 
 #include "Listener.h"
+
+using namespace std;
 
 Listener::Listener(LoginManager* loginManager) :
 		_loginManager(loginManager), _running(false), _clientSocket(new TCPSocket(MSNGR_PORT)) {
